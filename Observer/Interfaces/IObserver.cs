d46a52164f1faf036457172162fd33590b890ace
shehanpathi <shehan@randomsoftware.net>
2020-08-01 13:25:42 +0530

    init

---
+++ Observer/Interfaces/IObserver.cs
@@ -0,0 +1,8 @@
+﻿namespace RefactoringGuru.DesignPatterns.Observer.Conceptual
+{
+    public interface IObserver
+    {
+        // Receive update from subject
+        void Update(ISubject subject);
+    }
+}
---
+++ Observer/classes/ConcreteObserverB.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RefactoringGuru.DesignPatterns.Observer.Conceptual
+{
+    class ConcreteObserverB : IObserver
+    {
+        public void Update(ISubject subject)
+        {
+            if ((subject as Subject).State == 0 || (subject as Subject).State >= 2)
+            {
+                Console.WriteLine("ConcreteObserverB: Reacted to the event.");
+            }
+        }
+    }
+}
